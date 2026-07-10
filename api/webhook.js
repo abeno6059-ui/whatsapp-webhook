2dd476c76a83c3a4b76278313343bb08c5efdb3f
abeno6059-ui <abeno6059@gmail.com>
2026-07-10 21:44:55 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,4 @@
 export default async function handler(req, res) {
-  console.log("===== WEBHOOK CALLED =====");
-  console.log("METHOD:", req.method);
-
   if (req.method === "GET") {
     const VERIFY_TOKEN = "mon_token_123";
 
@@ -16,40 +13,21 @@ export default async function handler(req, res) {
     return res.status(403).send("Forbidden");
   }
 
- if (req.method === "POST") {
-  console.log("===== WEBHOOK CALLED =====");
-  const value = req.body?.entry?.[0]?.changes?.[0]?.value;
-
-console.log("HAS_MESSAGES:", !!value?.messages);
-console.log("HAS_STATUSES:", !!value?.statuses);
-
   if (req.method === "POST") {
-  console.log(
-  "EVENT:",
-  JSON.stringify(req.body.entry?.[0]?.changes?.[0]?.value, null, 2)
-);
+    console.log("===== WEBHOOK CALLED =====");
 
-  return res.status(200).send("OK");
-}
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req.body),
-      }
+    const value = req.body?.entry?.[0]?.changes?.[0]?.value;
+
+    console.log("HAS_MESSAGES:", !!value?.messages);
+    console.log("HAS_STATUSES:", !!value?.statuses);
+
+    console.log(
+      "EVENT:",
+      JSON.stringify(value, null, 2)
     );
 
-    const text = await response.text();
-
-    console.log("n8n Status:", response.status);
-    console.log("n8n Response:", text);
-
-  } catch (err) {
-    console.error("FETCH ERROR:", err);
+    return res.status(200).send("OK");
   }
-
-  return res.status(200).send("OK");
-}
 
   return res.status(405).send("Method Not Allowed");
 }
